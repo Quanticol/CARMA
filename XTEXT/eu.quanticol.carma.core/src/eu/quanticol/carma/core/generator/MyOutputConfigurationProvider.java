ad4f2a7f32563134241c54e5946fa05ac2d52159
C D Williams <c.d.williams@ed.ac.uk>
2015-06-03 14:40:44 +0100

    [FIX] changed generation output folder

---
+++ XTEXT/eu.quanticol.carma.core/src/eu/quanticol/carma/core/generator/MyOutputConfigurationProvider.java
@@ -0,0 +1,27 @@
+package eu.quanticol.carma.core.generator;
+
+import static com.google.common.collect.Sets.newHashSet;
+
+import java.util.Set;
+
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IOutputConfigurationProvider;
+import org.eclipse.xtext.generator.OutputConfiguration;
+
+public class MyOutputConfigurationProvider implements
+    IOutputConfigurationProvider {
+
+	/**
+	 * @return a set of {@link OutputConfiguration} available for the generator
+	 */
+	public Set<OutputConfiguration> getOutputConfigurations() {
+	    OutputConfiguration defaultOutput = new OutputConfiguration(IFileSystemAccess.DEFAULT_OUTPUT);
+	    defaultOutput.setDescription("Output Folder");
+	    defaultOutput.setOutputDirectory("./src");
+	    defaultOutput.setOverrideExistingResources(true);
+	    defaultOutput.setCreateOutputDirectory(true);
+	    defaultOutput.setCleanUpDerivedResources(true);
+	    defaultOutput.setSetDerivedProperty(true);
+	    return newHashSet(defaultOutput);
+	}
+}
