c0ea04703d1b50b37614340e340dba471d7a9129
Christopher D Williams <blasedef@gmail.com>
2015-08-24 18:06:25 +0100

    Wizard

--- UI/eu.quanticol.carma.ui/src/eu/quanticol/carma/ui/handlers/SimulationHandler.java
+++ UI/eu.quanticol.carma.ui/src/eu/quanticol/carma/ui/handlers/SimulationHandler.java
@@ -3,16 +3,14 @@
  */
 package eu.quanticol.carma.ui.handlers;
 
-import java.util.HashMap;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.window.Window;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
 
-import eu.quanticol.carma.core.carma.Model;
-import eu.quanticol.carma.core.ui.CarmaUiUtil;
-import eu.quanticol.carma.simulator.CarmaModel;
+import eu.quanticol.carma.ui.wizards.SimulationWizard;
 
 /**
  * @author loreti
@@ -21,26 +19,39 @@ import eu.quanticol.carma.simulator.CarmaModel;
 public class SimulationHandler extends AbstractHandler {
 
 	
-	CarmaUiUtil util = new CarmaUiUtil();
+//	CarmaUiUtil util = new CarmaUiUtil();
+//	
+//	@Override
+//	public Object execute(ExecutionEvent event) throws ExecutionException {
+//		System.out.println("HANDLER EXECUTION!!!!!");
+//		HashMap<IResource,CarmaModel> map = util.getActiveModels();
+//		for (IResource r: map.keySet()) {
+//			CarmaModel m = map.get(r);
+//			System.out.println("FILE: "+r.getName());
+//			System.out.println("SYSTEMS: ");
+//			String[] systems = m.getSystems();
+//			String[] measures = m.getMeasures();
+//			for( int i=0 ; i<systems.length ; i++ ) {
+//				System.out.println("- "+systems[i]);
+//			}
+//			System.out.println("MEASURES: ");
+//			for( int i=0 ; i<measures.length ; i++ ) {
+//				System.out.println("- "+measures[i]);
+//			}
+//		}
+//	    return true;
+//	}
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		System.out.println("HANDLER EXECUTION!!!!!");
-		HashMap<IResource,CarmaModel> map = util.getActiveModels();
-		for (IResource r: map.keySet()) {
-			CarmaModel m = map.get(r);
-			System.out.println("FILE: "+r.getName());
-			System.out.println("SYSTEMS: ");
-			String[] systems = m.getSystems();
-			String[] measures = m.getMeasures();
-			for( int i=0 ; i<systems.length ; i++ ) {
-				System.out.println("- "+systems[i]);
-			}
-			System.out.println("MEASURES: ");
-			for( int i=0 ; i<measures.length ; i++ ) {
-				System.out.println("- "+measures[i]);
-			}
-		}
+		
+	    WizardDialog wizardDialog = new WizardDialog(HandlerUtil.getActiveShell(event),
+	    	      new SimulationWizard());
+	    	    if (wizardDialog.open() == Window.OK) {
+	    	      System.out.println("Ok pressed");
+	    	    } else {
+	    	      System.out.println("Cancel pressed");
+	    	      }
 	    return true;
 	}
 	
--- UI/eu.quanticol.carma.ui/src/eu/quanticol/carma/ui/wizards/SimulationWizard.java
+++ UI/eu.quanticol.carma.ui/src/eu/quanticol/carma/ui/wizards/SimulationWizard.java
@@ -3,74 +3,379 @@
  */
 package eu.quanticol.carma.ui.wizards;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.CheckStateChangedEvent;
+import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.viewers.ICheckStateListener;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
 
-import eu.quanticol.carma.core.carma.Model;
 import eu.quanticol.carma.core.ui.CarmaUiUtil;
+import eu.quanticol.carma.simulator.CarmaModel;
 
 /**
- * @author loreti
+ * @author loreti, williams
  *
  */
-public class SimulationWizard {//extends Wizard {
+public class SimulationWizard extends Wizard {
 
-//	protected SelectModelAndSystemPage modelAndSystemPage;
-//	
-//	public SimulationWizard() {
-//		super();
-//		setNeedsProgressMonitor(true);
-//	}
-//	
-//	@Override
-//	public void addPages() {
-//		modelAndSystemPage = new SelectModelAndSystemPage();
-//		addPage(modelAndSystemPage);
-//	}
-//
-//	@Override
-//	public boolean performFinish() {
-//		return false;
-//	}
-//	
-//	
-//	public class SelectModelAndSystemPage extends WizardPage {
-//		
-//		private Composite container;
-//		private LinkedList<Model> models;
-//
-//		public SelectModelAndSystemPage() {
-//			super("Select Model and System");
-//			setTitle("Select Model and System");
-//			setDescription("Select Model and System");
-//		}		
-//		
-//		@Override
-//		public void createControl(Composite parent) {
-//		    this.models = CarmaUiUtils.getActiveModels();
-//			this.container = new Composite(parent, SWT.NONE);
-//			 GridLayout layout = new GridLayout();
-//			    container.setLayout(layout);
-//			    layout.numColumns = 2;
-//			    Label label1 = new Label(container, SWT.NONE);
-//			    label1.setText("Model:");
-//			    Combo combo = new Combo(container, SWT.NONE);
-//			    for (Model model : models) {
-//					combo.add(model.eResource().getURI().toFileString());
-//				}
-//			    setControl(container);
-//			    setPageComplete(false);
-//
-//		}
-//		
-//	}
+	CarmaUiUtil util = new CarmaUiUtil();
+	
+	private ArrayList<WizardPage> wizardPageList = new ArrayList<WizardPage>();
+	
+	protected SelectModelAndSystemPage modelAndSystemPage;
+	protected SelectMeasuresPage measuresPage;
+	protected SetDeadlineAndIterations deadlineAndIterations;
+	
+	protected int modelChoice = -1;
+	protected int systemChoice = -1;
+	HashMap<IResource,CarmaModel> models;
+	ArrayList<IResource> resources = new ArrayList<IResource>();
+	
+	public SimulationWizard() {
+		super();
+		this.models = util.getActiveModels();
+		this.resources.addAll(models.keySet());
+		setNeedsProgressMonitor(true);
+	}
+	
+	@Override
+	public void addPages() {
+		
+		modelAndSystemPage = new SelectModelAndSystemPage();
+		addPage(modelAndSystemPage);
+		wizardPageList.add(modelAndSystemPage);
+		
+		measuresPage = new SelectMeasuresPage();
+		addPage(measuresPage);
+		wizardPageList.add(measuresPage);
+		
+		deadlineAndIterations = new SetDeadlineAndIterations();
+		addPage(deadlineAndIterations);
+		wizardPageList.add(deadlineAndIterations);
+	}
 
+	@Override
+	public boolean performFinish() {
+		return false;
+	}
+	
+	@Override
+	public boolean canFinish(){
+		
+		boolean finished = true;
+		
+		for(WizardPage w : this.wizardPageList){
+			finished = finished && w.isPageComplete();
+		}
+		
+		return finished;
+		
+	}
+	
+	
+	public class SelectModelAndSystemPage extends WizardPage {
+		
+		private Composite container;
+		private String[] labels;
+		private Label modelLabel;
+		private Label systemLabel;
+		private Combo modelCombo;
+		private Combo systemCombo;
+
+		public SelectModelAndSystemPage() {
+			super("Select Model and System");
+			setTitle("Select Model and System");
+			setDescription("Select Model and System");
+		}
+
+		@Override
+		public void createControl(Composite parent) {
+			container = new Composite(parent, SWT.NONE);
+			GridLayout layout = new GridLayout();
+		    container.setLayout(layout);
+		    layout.numColumns = 2;
+		    
+		    drawPage();
+		    
+		    setControl(container);
+			    
+
+		}
+		
+		public void drawPage(){
+			
+		    modelLabel = new Label(container, SWT.NONE);
+		    modelLabel.setText("Model:");
+			modelCombo = new Combo(container, SWT.NONE);
+			
+			systemLabel = new Label(container, SWT.NONE);
+		    systemLabel.setText("System:");
+			systemCombo = new Combo(container, SWT.NONE);
+			
+		    for (IResource res : resources) {
+		    	modelCombo.add(res.getName().split("\\.")[0]);
+			}
+			
+			if(modelChoice > -1){
+				labels = models.get(resources.get(modelChoice)).getSystems();
+				modelCombo.setText(resources.get(modelChoice).getName().split("\\.")[0]);
+			} else
+		    	labels = new String[] {""};
+			
+		    for (String label : labels) {
+		    	systemCombo.add(label);
+			}
+
+		    modelCombo.addListener(SWT.Selection, new Listener() {
+				public void handleEvent(Event event) {
+					modelChoice = modelCombo.getSelectionIndex();
+					refreshPage();
+					checkPage();
+				}
+			});
+		    
+		    systemCombo.addListener(SWT.Selection, new Listener() {
+				public void handleEvent(Event event) {
+					systemChoice = systemCombo.getSelectionIndex();
+					checkPage();
+				}
+			});
+		    
+			
+		}
+		
+		public void refreshPage(){
+			
+			modelLabel.dispose();
+			modelCombo.dispose();
+			
+			systemLabel.dispose();
+			systemCombo.dispose();
+			
+			drawPage();
+		    
+		    container.layout(true);
+		}
+		
+		public void checkPage(){
+			
+			
+			if(modelChoice == -1){
+				setPageComplete(false);
+				return;
+			}
+			if(systemChoice == -1){
+				setPageComplete(false);
+				return;
+			}
+			setPageComplete(true);
+			measuresPage.updatePage();
+		}
+		
+	}
+	
+	public class SelectMeasuresPage extends WizardPage {
+		
+		private class MeasuresProvider extends LabelProvider implements ITableLabelProvider {
+
+			@Override
+			public Image getColumnImage(Object element, int columnIndex) {
+				return null;
+			}
+
+			@Override
+			public String getColumnText(Object element, int columnIndex) {
+				return (String) element;
+			}
+			
+		}
+		
+		private Composite container;
+		private CheckboxTableViewer viewer;
+
+		public SelectMeasuresPage() {
+			super("Select Measures");
+			setTitle("Select Measures");
+			setDescription("Select Measures");
+		}
+
+		@Override
+		public void createControl(Composite parent) {
+			container = new Composite(parent, SWT.NONE);
+			GridLayout layout = new GridLayout();
+		    container.setLayout(layout);
+
+		    createViewer(container);
+		    
+		    setControl(container);
+			    
+
+		}
+		
+		public void updatePage(){
+			
+			
+			viewer.setInput(getViewerInput());
+			
+			container.layout();
+		}
+		
+		public void createViewer(Composite parent){
+			
+			GridData gridData = new GridData(GridData.FILL_BOTH);
+		    gridData.horizontalSpan = 2;
+			
+			viewer = CheckboxTableViewer.newCheckList(parent, SWT.NONE);
+			viewer.getTable().setLayoutData(gridData);
+			viewer.setContentProvider(new ArrayContentProvider());
+			viewer.setLabelProvider(getProvider());
+			viewer.setInput(getViewerInput());
+			viewer.addCheckStateListener(new ICheckStateListener() {
+
+				public void checkStateChanged(CheckStateChangedEvent event) {
+					checkPage();
+				}
+
+			});
+			parent.pack();
+		}
+		
+		ITableLabelProvider getProvider() {
+			return new MeasuresProvider();
+		}
+		
+		Object getViewerInput() {
+			if(modelChoice > -1)
+				return models.get(resources.get(modelChoice)).getMeasures();
+			else
+				return new String[] {"null"};
+		}
+		
+		public void checkPage(){
+			if(viewer.getCheckedElements().length < 1){
+				setPageComplete(false);
+				return;
+			}
+			setPageComplete(true);
+		}  
+		
+	}
+	
+	public class SetDeadlineAndIterations extends WizardPage {
+		
+		private Composite container;
+		private PositiveIntegerConfigurationText deadline;
+		private PositiveIntegerConfigurationText iterations;
+		private Label deadlineLabel;
+		private Label iterationsLabel;
+		
+		
+		public class PositiveIntegerConfigurationText {
+			
+			Text control;
+			protected String propertyValue;
+			
+			public PositiveIntegerConfigurationText(String defaultValue) {
+				propertyValue = defaultValue;
+			}
+			
+			public Control createControl(Composite parent) {
+				control = new Text(parent, SWT.BORDER | SWT.RIGHT);
+				updateControl();
+				control.addListener(SWT.Modify, new Listener() {
+
+					public void handleEvent(Event event) {
+						setValue(((Text) control).getText());
+						checkPage();
+					}
+
+				});
+				return control;
+			}
+			
+			public
+			void setValue(String value) {
+				this.propertyValue = value;
+			}
+			
+			
+			public void updateControl() {
+				((Text) control).setText("" + getValue());
+			}
+			
+			public Object getValue() {
+				try {
+					return Integer.parseInt(this.propertyValue);
+				} catch (NumberFormatException e) {
+					return null;
+				}
+			}
+			
+			public boolean isValid() {
+				try {
+					int i = Integer.parseInt(this.propertyValue);
+					return i > 0;
+				} catch (NumberFormatException e) {
+					return false;
+				}
+			}
+		}
+
+		protected SetDeadlineAndIterations() {
+			super("Set deadline and iterations");
+			setTitle("Set deadline and iterations");
+			setDescription("Set deadline and iterations");
+		}
+
+		@Override
+		public void createControl(Composite parent) {
+			container = new Composite(parent, SWT.NONE);
+			GridLayout layout = new GridLayout();
+		    container.setLayout(layout);
+		    layout.numColumns = 2;
+		    
+		    drawPage(container);
+		    
+		    setControl(container);
+			
+		}
+		
+		public void drawPage(Composite container){
+			
+			deadlineLabel = new Label(container, SWT.NONE);
+			deadlineLabel.setText("Deadline:");
+			deadline = new PositiveIntegerConfigurationText("50");
+			deadline.createControl(container);
+			
+			iterationsLabel = new Label(container, SWT.NONE);
+			iterationsLabel.setText("Iterations:");
+			iterations = new PositiveIntegerConfigurationText("100");
+			iterations.createControl(container);
+			
+		}
+		
+		public void checkPage(){
+			setPageComplete(true);
+		}  
+		
+	}
+	
 }
